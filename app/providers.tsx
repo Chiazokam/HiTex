33151f746f2c98f93414366f1f0f3f0547d0d381
Chiazokam Echeta <chiazokamecheta@gmail.com>
2024-08-01 19:10:51 +0100

    fix: update toolbar
    - show toolbar on text selection
    - modify header

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,9 @@
-'use client'
-
-import { ChakraProvider } from '@chakra-ui/react'
+import { MantineProvider } from '@mantine/core'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return (
+      <MantineProvider>
+        {children}
+      </MantineProvider>
+  )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FileText, EllipsisVertical } from 'lucide-react';
-import { Divider, Button } from '@chakra-ui/react'
+import { Divider, Button } from '@mantine/core'
 import Contributors from '@/components/Contributors';
 import UserAvatar from '@/components/UserAvatar';
 
 const Header = () => {
     return (
-        <div className='flex border-b py-5 px-4 mb-4 justify-between'>
+        <div className='flex border-b fixed z-100 top-0 w-full border-gray-100 py-5 px-12 bg-white justify-between'>
             <div className='flex gap-3 items-center'>
                 <div className='border rounded-full bg-red-100 p-1 border-red-100'>
                     <FileText size={16} color='#BF6388' />
@@ -17,12 +17,12 @@ const Header = () => {
             <div className='flex gap-3 items-center'>
                 <Contributors className='hidden md:flex' />
                 <Divider orientation="vertical" className='hidden md:flex' />
-                <Button colorScheme='blue' size='sm' className='hidden md:flex'>Share</Button>
-                <div className='border rounded py-1.5 px-1'>
+                <Button size='sm' className='hidden md:flex'>Share</Button>
+                <div className='border rounded py-1.5 px-1 cursor-pointer border-gray-200'>
                     <EllipsisVertical size={15} />
                 </div>
                 <div className='cursor-pointer'>
-                    <UserAvatar image='' name='Ciara Noon' />
+                    <UserAvatar image='' />
                 </div>
             </div>
         </div>
