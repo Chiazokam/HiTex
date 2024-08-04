f5fc96ffffefec8ae1a10f9400a570daa9cb9aed
Chiazokam Echeta <chiazokamecheta@gmail.com>
2024-08-04 23:44:20 +0100

    feat: fix layout and scroll
    - fix editor showing under Header
    - implement author box partially

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Editor from '@/components/Editor'
 import Header from '@/components/Header';
+import AuthorBox from '@/components/AuthorBox';
 
 const Layout = () => {
 
     return (
         <div className='flex h-screen flex-col'>
             <Header />
-            <div className='mt-40 px-44'>
+            <div className='mt-[64px] px-12 md:px-36 lg:px-60 xl:px-96 py-12 overflow-scroll'>
+                <AuthorBox />
                 <Editor />
             </div>
         </div>
