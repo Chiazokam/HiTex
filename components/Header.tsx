8fb54b09fb0f6b2e982756cffad100d35ce651fe
Chiazokam Echeta <chiazokamecheta@gmail.com>
2024-10-23 13:02:59 +0100

    fix: use mantine layout styles
    - fix header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,22 +15,22 @@ const Header = () => {
         <header className='flex border-b fixed z-2 top-0 w-full shadow-lg border-gray-100 py-5 px-4 sm:px-6 bg-white justify-between h-[64px]'>
             <div className='flex gap-3 items-center'>
                 <div className='border rounded-full bg-red-100 p-1 border-red-100'>
-                    <DocumentTextIcon className='w-4 h-4' color='#BF6388' />
+                    <DocumentTextIcon className='w-4 h-4 text-red-700' />
                 </div>
                 <span className='font-thin w-48 truncate sm:w-full'>Seven Fundamental Design Principles</span>
             </div>
 
             <div className='flex gap-2 items-center'>
                 <Contributors className='hidden sm:flex' />
-                <Divider orientation="vertical" className='hidden sm:flex' />
+                <Divider orientation="vertical" className='hidden sm:flex w-[0.01rem] bg-zinc-300' />
                 
-                <Button className='!hidden sm:!flex' size='sm' color='blue.5' onClick={open}>Share</Button>
-                <Share2Icon fontWeight={1} className='!flex sm:!hidden w-5 h-5 text-gray-500 font-thin' onClick={open} />
+                <Button className='hidden sm:flex px-5 py-1 bg-sky-500 text-white text-sm font-semibold' onClick={open}>Share</Button>
+                <Share2Icon fontWeight={1} className='flex sm:hidden w-5 h-5 text-gray-500 font-thin' onClick={open} />
                 
                 <ShareDocument opened={opened} close={close} />
 
-                <EllipsisVerticalIcon className='w-4 h-4 !flex sm:!hidden text-gray-500' />
-                <div className='cursor-pointer !hidden sm:!flex'>
+                <EllipsisVerticalIcon className='w-4 h-4 flex sm:hidden text-gray-500' />
+                <div className='cursor-pointer hidden sm:flex'>
                     <UserAvatar image='' name='Dylan Mirror' />
                 </div>
             </div>
